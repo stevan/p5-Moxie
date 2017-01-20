2c6f5c022b7f0e5182841aa5cdd81fbe5c21857d
Stevan Little <stevan.little@booking.com>
2017-01-20 14:13:28 +0100

    re-write the whole trait system (and include it in the commit)

---
+++ lib/Moxie/Trait.pm
@@ -0,0 +1,210 @@
+package Moxie::Trait;
+# ABSTRACT: Traits system
+
+use v5.22;
+use warnings;
+use experimental qw[
+    signatures
+    postderef
+];
+
+our $VERSION   = '0.01';
+our $AUTHORITY = 'cpan:STEVAN';
+
+sub ro ( $meta, $method_name, @args ) {
+
+    my $slot_name;
+    if ( $args[0] ) {
+        $slot_name = shift @args;
+    }
+    else {
+        if ( $method_name =~ /^get_(.*)$/ ) {
+            $slot_name = $1;
+        }
+        else {
+            $slot_name = $method_name;
+        }
+    }
+
+    die 'Unable to find slot `' . $slot_name.'` in `'.$meta->name.'`'
+        unless $meta->has_slot( $slot_name )
+            || $meta->has_slot_alias( $slot_name );
+
+    $meta->add_method( $method_name => sub {
+        die "Cannot assign to `$slot_name`, it is a readonly" if scalar @_ != 1;
+        $_[0]->{ $slot_name };
+    });
+}
+
+sub rw ( $meta, $method_name, @args ) {
+
+    my $slot_name;
+    if ( $args[0] ) {
+        $slot_name = shift @args;
+    }
+    else {
+        $slot_name = $method_name;
+    }
+
+    die 'Unable to find slot `' . $slot_name.'` in `'.$meta->name.'`'
+        unless $meta->has_slot( $slot_name )
+            || $meta->has_slot_alias( $slot_name );
+
+    $meta->add_method( $method_name => sub {
+        $_[0]->{ $slot_name } = $_[1] if scalar( @_ ) > 1;
+        $_[0]->{ $slot_name };
+    });
+}
+
+sub wo ( $meta, $method_name, @args ) {
+
+    my $slot_name;
+    if ( $args[0] ) {
+        $slot_name = shift @args;
+    }
+    else {
+        if ( $method_name =~ /^set_(.*)$/ ) {
+            $slot_name = $1;
+        }
+        else {
+            $slot_name = $method_name;
+        }
+    }
+
+    die 'Unable to find slot `' . $slot_name.'` in `'.$meta->name.'`'
+        unless $meta->has_slot( $slot_name )
+            || $meta->has_slot_alias( $slot_name );
+
+    $meta->add_method( $method_name => sub {
+        die "You must supply a value to write to `$slot_name`" if scalar(@_) < 1;
+        $_[0]->{ $slot_name } = $_[1];
+    });
+}
+
+sub predicate ( $meta, $method_name, @args ) {
+
+    my $slot_name;
+    if ( $args[0] ) {
+        $slot_name = shift @args;
+    }
+    else {
+        if ( $method_name =~ /^has_(.*)$/ ) {
+            $slot_name = $1;
+        }
+        else {
+            $slot_name = $method_name;
+        }
+    }
+
+    die 'Unable to find slot `' . $slot_name.'` in `'.$meta->name.'`'
+        unless $meta->has_slot( $slot_name )
+            || $meta->has_slot_alias( $slot_name );
+
+    $meta->add_method( $method_name => sub { defined $_[0]->{ $slot_name } } );
+}
+
+sub clearer ( $meta, $method_name, @args ) {
+
+    my $slot_name;
+    if ( $args[0] ) {
+        $slot_name = shift @args;
+    }
+    else {
+        if ( $method_name =~ /^clear_(.*)$/ ) {
+            $slot_name = $1;
+        }
+        else {
+            $slot_name = $method_name;
+        }
+    }
+
+    die 'Unable to find slot `' . $slot_name.'` in `'.$meta->name.'`'
+        unless $meta->has_slot( $slot_name )
+            || $meta->has_slot_alias( $slot_name );
+
+    $meta->add_method( $method_name => sub { undef $_[0]->{ $slot_name } } );
+}
+
+sub private ( $meta, $method_name, @args ) {
+
+    my $slot_name;
+    if ( $args[0] ) {
+        $slot_name = shift @args;
+    }
+    else {
+        $slot_name = $method_name;
+    }
+
+    die 'Unable to find slot `' . $slot_name.'` in `'.$meta->name.'`'
+        unless $meta->has_slot( $slot_name )
+            || $meta->has_slot_alias( $slot_name );
+
+    # NOTE:
+    # These are lexical accessors ...
+
+    # we should not be able to find it in the symbol table ...
+    if ( $meta->has_method( $method_name ) || $meta->has_method_alias( $method_name ) || $meta->requires_method( $method_name ) ) {
+        die 'Trying to install private (lexical) accessor for slot('.$slot_name.') named ('
+            .$method_name.') and found a conflicting non-lexical method of that name';
+    }
+    else {
+        # at this point we can assume that we have a lexical
+        # method which we need to transform, and in order to
+        # do that we need to look at all the methods in this
+        # class and find all the ones who 'close over' the
+        # lexical method and then re-write their lexical pad
+        # to use the accessor method that I will generate.
+
+        # NOTE:
+        # we need to delay this until the UNITCHECK phase
+        # because we need all the methods of this class to
+        # have been compiled, at this moment, they are not.
+        B::CompilerPhase::Hook::enqueue_UNITCHECK {
+            # now check the class local methods ....
+            foreach my $m ( $meta->methods ) {
+                # get a HASH of the things the method closes over
+                my $closed_over = PadWalker::closed_over( $m->body );
+
+                #warn Data::Dumper::Dumper({
+                #    class       => $meta->name,
+                #    method      => $m->name,
+                #    closed_over => $closed_over,
+                #    trait       => $trait,
+                #    looking_for => $method_name,
+                #});
+
+                # if the private method is used, then it will be
+                # here with a prepended `&` sigil ...
+                if ( exists $closed_over->{ '&' . $method_name } ) {
+                    # now we know that we have someone using the
+                    # lexical method inside the method body, so
+                    # we need to generate our accessor accordingly
+                    # then this is as simple as assigning the HASH key
+                    $closed_over->{ '&' . $method_name } =  sub {
+                        package DB; @DB::args = (); my () = caller(1);
+                        my ($self)  = @DB::args;
+                        $self->{ $slot_name } = $_[0] if scalar @_;
+                        $self->{ $slot_name };
+                    };
+
+                    # okay, now restore the closed over vars
+                    # with our new addition...
+                    PadWalker::set_closed_over( $m->body, $closed_over );
+                }
+            }
+        };
+    }
+
+}
+
+1;
+
+__END__
+
+=pod
+
+=head1 DESCRIPTION
+
+This is where the traits are stored.
+
+=cut
---
+++ lib/Moxie/Trait/Util.pm
@@ -0,0 +1,126 @@
+
+package Moxie::Trait::Util;
+# ABSTRACT: Traits system
+
+use v5.22;
+use warnings;
+use experimental qw[
+    signatures
+    postderef
+];
+
+our $VERSION   = '0.01';
+our $AUTHORITY = 'cpan:STEVAN';
+
+## ...
+
+our %__CODE_ATTRIBUTE_STORAGE__;
+
+sub SCHEDULE_TRAIT_COLLECTION ( $meta ) {
+
+    # It does not make any sense to create
+    # something that is meant to run in the
+    # BEGIN phase *after* that phase is done
+    # so catch this and error ...
+    die 'Trait collection must be scheduled during BEGIN time, not (' . ${^GLOBAL_PHASE}. ')'
+        unless ${^GLOBAL_PHASE} eq 'START';
+
+    # This next step, we want to do
+    # immediately after this method
+    # (and the BEGIN block it is
+    # contained within) finishes.
+
+    # Since these are BEGIN blocks,
+    # they need to be enqueued in
+    # the reverse order they will
+    # run in order to have the method
+    # not trip up role composiiton
+    B::CompilerPhase::Hook::enqueue_BEGIN {
+        # we remove the modifier, but leave
+        # the fetcher because that is how
+        # attributes::get will find this info
+        $meta->delete_method_alias('MODIFY_CODE_ATTRIBUTES')
+    };
+    B::CompilerPhase::Hook::enqueue_BEGIN {
+        $meta->alias_method(
+            FETCH_CODE_ATTRIBUTES => sub ($pkg, $code) {
+                return unless exists $__CODE_ATTRIBUTE_STORAGE__{ $code };
+                return $__CODE_ATTRIBUTE_STORAGE__{ $code }->@*;
+            }
+        );
+        $meta->alias_method(
+            MODIFY_CODE_ATTRIBUTES => sub ($pkg, $code, @attrs) {
+
+                # First lets parse the traits, currently
+                # we are not terribly sophisticated, but
+                # we accept `foo` calls (no-parens) and
+                # we accept `foo(1, 2, 3)` calls (parens
+                # with comma seperated args).
+
+                # NOTE:
+                # None of the args are eval-ed and they are
+                # basically just a list of strings.
+                my @traits = map {
+                    if ( m/^([a-z_]*)\((.*)\)$/ ) {
+                        [ $1, [ split /\,/ => $2 ], $_ ]
+                    }
+                    elsif ( m/^([a-z_]*)$/ ) {
+                        [ $1, [], $_ ]
+                    }
+                } @attrs;
+
+                #use Data::Dumper;
+                #warn Dumper \@traits;
+
+                # Now loop through the traits and look to
+                # see if we have any ones we cannot handle
+                # and collect them for later ...
+                my @bad = grep not(Moxie::Trait->can( $_->[0] )), @traits;
+
+                #use Data::Dumper;
+                #warn Dumper \@bad;
+
+                # bad traits are bad, return the originals
+                # that we do not handle
+                return map $_->[2], @bad if @bad;
+
+                # now we need to loop through the traits
+                # that we parsed and apply the trait function
+                # to our method accordingly
+
+                my $method      = MOP::Method->new( $code );
+                my $method_name = $method->name;
+                foreach my $trait ( @traits ) {
+                    my ($t, $args) = @$trait;
+                    Moxie::Trait->can( $t )->( $meta, $method_name, @$args );
+                }
+
+                # next we need to fetch the latest version
+                # of the method installed in the stash, or
+                # if that cannot be found, use the original
+                # one, and we then need to store the info
+                # about the traits so it can be retrieved
+                # via attributes::get
+
+                if ( my $generated = $meta->get_method( $method->name ) || $method ) {
+                    $__CODE_ATTRIBUTE_STORAGE__{ $generated->body } = \@traits;
+                }
+
+                # all is well, so let the world know that ...
+                return;
+            }
+        );
+    };
+}
+
+1;
+
+__END__
+
+=pod
+
+=head1 DESCRIPTION
+
+Nothing to see here
+
+=cut
